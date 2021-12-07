09e3ed7c1776e25112f554413f4b982e8109e992
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-07 14:22:18 -0500

    Populate the DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,5 +3,16 @@ import React from "react";
 import DayListItem from 'components/DayListItem.js';
 
 export default function DayList(props) {
-  return <ul></ul>
-}
+  const days = props.days.map(day => {
+    return (
+      <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+      />
+    );
+  });
+  return <ul>{days}</ul>;
+}; 

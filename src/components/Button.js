6a0b47a7fb986e60dffb03c3f6bd3e939aec0154
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-07 10:49:42 -0500

    Refactor the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,13 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-  let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-
-  if (props.danger) {
-     buttonClass += " button--danger";
-  }
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
 
   return (
     <button
@@ -22,6 +18,4 @@ export default function Button(props) {
       {props.children}
     </button>
   );
-
-  return <button className={buttonClass}>{props.children}</button>;
 }

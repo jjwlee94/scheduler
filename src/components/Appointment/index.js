cbb293a00de9e968aa20c71d2c32ae0c10ddcfb7
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-08 17:34:47 -0500

    Complete displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,22 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
-    <>
-      {!props.time && (
-        <article className="appointment"> No Appointment</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
       )}
-      {props.time && (
-        <article className="appointment">Appointment at {props.time}</article>
-      )}
-    </>
+    </article>
   );
 }

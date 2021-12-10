5fd2f65a3ddbc0826c6dbfc5e46f3d72cc296ac3
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-10 12:45:54 -0500

    Build a custom Hook to manage the visual mode of the Appointment component

---
+++ hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const popHistory = () =>
+    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      popHistory;
+    }
+    setMode(newMode);
+    setHistory((prev) => [...prev, newMode]);
+  };
+
+  const back = () => {
+    if (history.length === 1) {
+      return;
+    }
+    setMode(history[history.length - 2]);
+    popHistory();
+  };
+
+  return { mode, transition, back };
+}
